3d57e4acd8afdf7605ee32a5593f72519ae091dc
PatuNL <rhvdamme@gmail.com>
2017-05-21 23:24:24 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+Python 3.6.1 (default, Mar 22 2017, 06:17:05) 
+[GCC 6.3.0 20170321] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=============================== RESTART: Shell ===============================
+>>> print("Hello")
+Hello
+>>> 
+=============================== RESTART: Shell ===============================
+>>> print("Hello World")
+[DEBUG ON]
+>>> 
+[DEBUG OFF]
+>>> 
